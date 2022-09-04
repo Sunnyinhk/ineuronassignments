09d4a3db278fc0e2fc43eb2904bf384405c3528f
Sunnyinhk <100104668+Sunnyinhk@users.noreply.github.com>
2022-09-04 09:05:23 +0800

    Add files via upload

---
+++ Assignment4.js
@@ -0,0 +1,22 @@
+﻿// JavaScript source code
+
+
+//Create a function that takes a number(step) as an argument and returns the
+//number of matchsticks in that step.See step 1, 2 and 3 in the image above.
+//Take input from the user in the function parameter and return the output
+//using the return statement.
+
+  //  examples -
+    //matchHouses(1) ➞ 6
+
+//matchHouses(4) ➞ 21
+//matchHouses(87) ➞ 436
+// patterrn is No. of matchhouses times 5 plus 1
+
+function matchHouses(x) {
+    matchsticks = (x * 5) + 1
+    console.log(matchsticks);
+}
+matchHouses(1);
+matchHouses(4);
+matchHouses(87);
